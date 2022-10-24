68e0656f61e242beb612d1eb7d8e49ad4d2cc9f8
efriman <elias.friman@gmail.com>
2022-10-24 15:24:13 +0100

    Added scripts

---
+++ scripts/GEO_query_ES.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+require(GEOquery)
+
+factors = read.table(file = "/exports/igmm/eddie/wendy-lab/ilia/mouse_factor_full_QC.txt", sep = "\t", header = T, fill = TRUE)
+
+factors <- factors[factors$Cell_type == "Embryonic Stem Cell",]
+factors <- factors[(factors$FastQC > 25) & (factors$UniquelyMappedRatio > 0.6) & (factors$PBC > 0.8) & (factors$PeaksFoldChangeAbove10 > 500) & (factors$FRiP > 0.01) & (factors$PeaksUnionDHSRatio > 0.7),]
+factors <- factors[complete.cases(factors),]
+
+GEOdata <- lapply(factors$GSMID, getGEO)
+
+factors$Sample <- ""
+factors$GSEID <- ""
+
+for (i in 1:length(GEOdata)) {
+  factors[factors$GSMID == Meta(GEOdata[[i]])$geo_accession,]$Sample <- Meta(GEOdata[[i]])$title
+  factors[factors$GSMID == Meta(GEOdata[[i]])$geo_accession,]$GSEID <- Meta(GEOdata[[i]])$series_id[[1]]
+}
+
+write.table(factors, "mouse_factor_ES.txt", quote=FALSE, sep="\t")
---
+++ scripts/GEO_query_GM12878.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+require(GEOquery)
+
+factors = read.table(file = "/exports/igmm/eddie/wendy-lab/ilia/human_factor/human_factor_full_QC.txt", sep = "\t", header = T, fill = TRUE)
+
+factors <- factors[factors$Cell_line == "GM12878",]
+factors <- factors[(factors$FastQC > 25) & (factors$UniquelyMappedRatio > 0.6) & (factors$PBC > 0.8) & (factors$PeaksFoldChangeAbove10 > 500) & (factors$FRiP > 0.01) & (factors$PeaksUnionDHSRatio > 0.7),]
+factors <- factors[complete.cases(factors),]
+
+GEOdata <- lapply(factors$GSMID, getGEO)
+
+factors$Sample <- ""
+factors$GSEID <- ""
+
+for (i in 1:length(GEOdata)) {
+  factors[factors$GSMID == Meta(GEOdata[[i]])$geo_accession,]$Sample <- Meta(GEOdata[[i]])$title
+  factors[factors$GSMID == Meta(GEOdata[[i]])$geo_accession,]$GSEID <- Meta(GEOdata[[i]])$series_id[[1]]
+}
+
+write.table(factors, "human_factor_GM12878.txt", quote=FALSE, sep="\t")
